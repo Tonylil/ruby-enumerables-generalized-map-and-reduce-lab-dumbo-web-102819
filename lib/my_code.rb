98f248d683cec11eb028020dfa6821244bcbe356
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-09 14:00:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,10 @@
 def map(array1)
   new = []
   i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
+  while i < source_array.length d
+    tempvar = source_array[i]
+    yield(tempvar)
+    new.push(tempvar) 
     i += 1
   end
   return new
