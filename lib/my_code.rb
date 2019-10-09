81cda5f7d0b1e388d10476455bb3a96f79dda992
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-10-09 13:59:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,11 @@
 # Your Code Here
 
 def map(array1)
-  yield(array1)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
 end
